84f726abd7f48788594088b1ca424c4fc1abe3eb
Klaercio <klaerciosilva@gmail.com>
2022-07-20 23:15:06 -0300

    🚀Menu adicionado

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,46 @@
 import IMenu from "../../interfaces/IMenu";
 import style from './Menu.module.scss';
 
-export default function Menu(menu: IMenu[]) {
+export default function Menu() {
+
+    const menu = [{
+        nome: 'Cadastro',
+        subMenus: [{
+            submenu: 'Produto',
+            linkSubmenu: 'localhost/cadastro/produto',
+        }, {
+            submenu: 'Cliente',
+            linkSubmenu: 'localhost/cadastro/cliente',
+        },
+        {
+            submenu: 'Funcionário',
+            linkSubmenu: 'localhost/cadastro/funcionário',
+        },
+        {
+            submenu: 'Fornecedor',
+            linkSubmenu: 'localhost/cadastro/fornecedor',
+        }],
+    }, 
+    {
+        nome: 'Estoque',
+        subMenus: [{
+            submenu: 'Consulta Produto',
+            linkSubmenu: 'localhost/cadastro/consultaProduto',
+        }]
+    },
+    {
+        nome: 'Relatórios',
+        subMenus: [{
+            submenu: 'Vendas por produto',
+            linkSubmenu: 'localhost/cadastro/vendasPorProduto',
+        }]
+    }];
+
     return (
         <div className={style.menu}>
             <ul>
                 {menu.map(item => {
-                  return( <li> <strong>{item.nome}</strong> {item.submMenus.map(itemSub => { return <li>itemSub.submenu</li>})}</li>)
+                  return( <li className={style.item}> <strong>{item.nome}</strong> <ul>{item.subMenus.map(itemSub => { return <li className={style.item}>{itemSub.submenu}</li>})}</ul></li>)
                 })}
             </ul>
         </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,12 @@
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
-import { menu } from '../../components/Menu/menu';
+
 export default function Home() {
 
     return (
         <div>
             <Header nome='google' razao='google'/>
-            <Menu menu={menu}></Menu>
-            Teste  de aparecimento
+            <Menu></Menu>
         </div>
     );
 }
